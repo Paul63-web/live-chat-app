ade847fb4d7eb4005e921d6a499fc518ed8a5838
Paul Oluwaseyi <paulisola784@gmail.com>
2022-05-01 17:00:08 +0100

    I updated my auth.service.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   baseUrl: "http://localhost:5034/api/account/",
   userBaseUrl: "http://localhost:5034/api/user/"
 };
